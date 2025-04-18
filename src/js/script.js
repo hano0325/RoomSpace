e6dbf58ba68beb799115235ee49054fdffe473b0
hano <hano.13242525@gmail.com>
2025-04-18 20:19:02 +0900

    newsからnews詳細まで

--- src/js/script.js
+++ src/js/script.js
@@ -204,6 +204,24 @@ document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', activateOnScroll);
 });
 
+$(function () {
+  // ヘッダーの高さ分だけコンテンツを下げる
+  const height = $(".js-header").height();
+  $("main").css("margin-top", height);
+
+  // ヘッダーの高さ取得
+  const headerHeight = $(".js-header").height();
+  $('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ヘッダーの高さ分下げる
+    let position = target.offset().top - headerHeight;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
+
 
 
 
